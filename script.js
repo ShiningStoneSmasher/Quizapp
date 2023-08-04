6abdb8b788af821445724b76b4cfe2d393bb2578
ShiningStoneSmasher <klarlhauertstein@gmail.com>
2023-08-04 14:20:02 +0200

    questions

---
+++ script.js
@@ -0,0 +1,83 @@
+let questions = [
+
+    {
+        "question_1": "Was ist der Unterschied zwischen Compiler und Interpreter?",
+        "answer_1": "Compiler übersetzen den gesamten Quellcode in Maschinencode, während Interpreter den Code Zeile für Zeile ausführt",
+        "answer_2": "blabla",
+        "answer_3": "blabla",
+        "answer_4": "blabla",
+        "right_answer": 2
+    },     
+    {
+        "question_2": 'Welche Programmiersprache wird oft als "Hello World" - Sprache für Anfänger verwendet?',
+        "answer_1": "Python",
+        "answer_2": "blabla",
+        "answer_3": "blabla",
+        "answer_4": "blabla",
+        "right_answer": 2
+    }, 
+    {
+        "question_3": 'Was bedeutet die Abkürzung "HTML" in Bezug auf Webentwicklung?',
+        "answer_1": "HyperText Markup Language",
+        "answer_2": "blabla",
+        "answer_3": "blabla",
+        "answer_4": "blabla",
+        "right_answer": 2
+    }, 
+    {
+        "question_4": "Welche Schleife wird mindestens einmal ausgeführt, auch wenn die Bedingung zu Beginn falsch ist?",
+        "answer_1": "Do-While-Schleife",
+        "answer_2": "blabla",
+        "answer_3": "blabla",
+        "answer_4": "blabla",
+        "right_answer": 2
+    }, 
+    {
+        "question_5": "Was ist der Wert von x nach der Ausführung dieses Codes: int x = 5; x = x++;?",
+        "answer_1": "5 (Der Post-Inkrement-Operator gibt den ursprünglichen Wert zurück, bevor er erhöht wird.)",
+        "answer_2": "blabla",
+        "answer_3": "blabla",
+        "answer_4": "blabla",
+        "right_answer": 2
+    }, 
+    {
+        "question_6": "Welche Datenstruktur wird verwendet, um Elemente in einer geordneten Sammlung ohne Duplikate zu speichern?",
+        "answer_1": "Set",
+        "answer_2": "blabla",
+        "answer_3": "blabla",
+        "answer_4": "blabla",
+        "right_answer": 2
+    }, 
+    {
+        "question_7": 'Was wird durch den Begriff "Scope" in der Programmierung beschrieben?',
+        "answer_1": "Den Bereich, in dem eine Variable sichtbar und zugänglich ist.",
+        "answer_2": "blabla",
+        "answer_3": "blabla",
+        "answer_4": "blabla",
+        "right_answer": 2
+    }, 
+    {
+        "question_8": 'Was ist der Unterschied zwischen "==" und "===" in JavaScript?',
+        "answer_1": '"==" prüft auf Gleichheit, wobei die Typen umgewandelt werden, während "===" sowohl den Wert als auch den Typ überprüft.',
+        "answer_2": "blabla",
+        "answer_3": "blabla",
+        "answer_4": "blabla",
+        "right_answer": 2
+    },
+    {
+        "question_9": "Was ist ein 404-Fehler in Bezug auf Webseiten?",
+        "answer_1": 'Ein Fehler, der auftritt, wenn die angeforderte Ressource nicht gefunden wurde.',
+        "answer_2": "blabla",
+        "answer_3": "blabla",
+        "answer_4": "blabla",
+        "right_answer": 2
+    }, 
+    {
+        "question_10": "Welche Art von Schleife wird verwendet, um eine Sammlung von Elementen zu durchlaufen, wie z.B. eine Liste?",
+        "answer_1": 'For-Schleife',
+        "answer_2": "blabla",
+        "answer_3": "blabla",
+        "answer_4": "blabla",
+        "right_answer": 2
+    } 
+];
